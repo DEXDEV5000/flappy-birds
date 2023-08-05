b758acb42d3c4809cfc857c267174f7839f3643c
DEXDEV5000 <gabrisariel74@gmail.com>
2023-08-05 17:50:28 -0300

    agrege imagenes, librerias y ventanas

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hola mundo")
+from tkinter import *
+Ventana=Tk()
+imagen_de_fondo=PhotoImage(file="imagenes\Kevin.png")
+background_label = Label(image=imagen_de_fondo)
+background_label.place(relwidth=1, relheight=1)  
+Ventana.title("Metal Men")
+Ventana.geometry("650x650")
+Ventana.config(bg="green")
+
+label3 = Label(Ventana,text="¡Hola Mundo!",bg="yellow")
+label3.config(height=1,width=18)
+label3.pack()
+Ventana.mainloop()
